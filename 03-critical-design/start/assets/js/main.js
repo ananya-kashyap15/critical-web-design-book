9b83e9c9f00fa959591527fa2c039409faece563
ananya-kashyap15 <123014825+ananya-kashyap15@users.noreply.github.com>
2023-03-01 11:25:30 -0600

    Added images

--- 03-critical-design/start/assets/js/main.js
+++ 03-critical-design/start/assets/js/main.js
@@ -4,39 +4,39 @@
 
 // 2
 document.getElementById("ele2").addEventListener("click", function () {
-  if (this.src.includes("cat-01")){
-    this.src = "assets/img/cat-02.jpg";
+  if (this.src.includes("Logo1-rebradn1")){
+    this.src = "assets/img/logo1.png";
   } else {
-    this.src = "assets/img/cat-01.jpg";
+    this.src = "assets/img/Logo1-rebrand1.png";
   }
 });
 
 // 3
 let ele3 = document.querySelector("#ele3");
 ele3.addEventListener("click", function () {
-  if (this.src.includes("cat-01")){
-    this.src = "assets/img/cat-02.jpg";
+  if (this.src.includes("Logo1-rebrand1")){
+    this.src = "assets/img/Logo1-rebrand1.png";
   } else {
-    this.src = "assets/img/cat-01.jpg";
+    this.src = "assets/img/logo1.png";
   }
 });
 
 // 4
 let ele4 = document.querySelector("#ele4");
 ele4.addEventListener("mouseover", function () {
-  this.src = "assets/img/cat-02.jpg";
+  this.src = "assets/img/Logo1-rebrand1.png";
 });
 ele4.addEventListener("mouseout", function () {
-  this.src = "assets/img/cat-01.jpg";
+  this.src = "assets/img/logo1.png";
 });
 
 // 5
 let ele5 = document.querySelector("#ele5");
 ele5.addEventListener("mouseover", function () {
-  this.style.backgroundImage = "url(assets/img/cat-02.jpg)";
+  this.style.backgroundImage = "url(assets/img/Logo1-renbrand1.png)";
 });
 ele5.addEventListener("mouseout", function () {
-  this.style.backgroundImage = "url(assets/img/cat-01.jpg)";
+  this.style.backgroundImage = "url(assets/img/logo1.png)";
 });
 
 
